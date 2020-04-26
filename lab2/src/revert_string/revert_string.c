28d2843d1a4a604bf15803f1f60ddf3b356838de
Shok1r <shokorovn@gmail.com>
2020-04-27 01:41:12 +0300

    Добавлен код

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,15 @@
 #include "revert_string.h"
 
-void RevertString(char *str)
-{
-	// your code here
+void RevertString(char *str){
+	
+	unsigned int len = strlen(str);
+	char *newstr = malloc(sizeof(char) * (len + 1));
+	for(int i = 0; i < len; i++){
+		*(newstr + i) = *(str + len - 1 - i);
+		}
+	for(int i = 0; i < len; i++){
+		*(str + i) = *(newstr + i);
+		}
+	free(newstr);
 }
 
